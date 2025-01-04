7156e41023aa8231c40f1fcddce6a6c5a93449fc
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-01-04 00:03:52 -0300

    side bar in to do list page

--- src/pages/PageTodo.jsx
+++ src/pages/PageTodo.jsx
@@ -3,15 +3,15 @@ import NavBar from "../components/navbar";
 function PageTodo() {
     return (
         <div id="appTodo" className="bg-cover h-[100vh]">
-           <div id="header" className='h-[100vh] w-[300px] ml-0 flex-col bg-black sticky border-white border-r-2'>
-              <h1 className="text-white text-3xl ml-10 mt-10">Cortex To-Do</h1>
+           <div id="header" className='h-[100vh] w-[300px] flex-col left-0 top-0 bg-black fixed border-white border-r-2 z-10'>
+              <h1 className="text-white text-3xl mt-10 ml-10">Cortex To-Do</h1>
               <nav className="mt-20">
                <NavBar/>
               </nav>
                  
            </div>
 
-           <div id="interface-to-do" className="mt-[170px]">
+           <div id="interface-to-do" className="mt-[120px]">
             <div className="text-center">
                 <input type="text" placeholder="Digite uma tarefa" className="w-[250px] p-[10px] rounded-full outline-none"/>
 
