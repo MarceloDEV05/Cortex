1df552ca28c52d411c6a47f0c01390d89a11f58a
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-02-23 15:57:45 -0300

    entendendo hooks do react

---
+++ src/components/boxForTask.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function boxForTask (){
+
+    
+
+    return(
+
+
+<div id="boxForTask" className={`z-99 bg-black/80 h-full w-full justify-center align-middle left-0 top-0 fixed`}>
+<div id="boxTask" className="w-[450px] h-[560px] bg-white m-auto mt-[90px] rounded-[20px]">
+    <div className="pt-14">
+        <h1 className="text-center font-bold text-3xl">Tarefas</h1>
+
+        <aside id="inputs" className="mt-20 columns-1 text-center align-middle">
+            <h1>Descreva sua tarefa:</h1><br />
+            <input type="text" className="border-2 w-[290px] pt-[10px] p-[10px] rounded-[8px]" placeholder="Nome da tarefa" /><br />
+            
+            <input type="text" className="border-2 w-[290px] mt-[15px] pt-[10px] p-[10px] rounded-[8px]" placeholder="Descrição da tarefa (*opcional)" />
+        </aside>
+
+        <article id="buttons" className="flex justify-around mt-10">
+           <div id="btnAdd">
+            <button className="border-2 w-40 p-[10px] rounded-[8px]">adicionar</button>
+           </div>
+
+            <div id="btnCancel">
+                <button className="border-2 w-40 p-[10px] rounded-[8px]">cancelar</button>
+            </div>
+
+        </article>
+
+    </div>
+</div>
+</div>
+
+);
+
+}
+
+export default boxForTask;
--- src/pages/PageTodo.jsx
+++ src/pages/PageTodo.jsx
@@ -1,14 +1,18 @@
 import NavBar from "../components/navbar";
 import pngcentral from "../assets/1187525.png";
+import { useState} from 'react'
+import boxForTask from "../components/boxForTask";
 
 
 
 function PageTodo() {
 
 
+
+
     return (
-        <div id="appTodo" className="bg-cover h-[100vh]">
-            <div id="header" className='h-[100vh] w-[300px] flex-col top-0 left-0 fixed bg-black border-white border-r-2 '>
+        <div id="appTodo" className="h-screen w-full ">
+            <div id="header" className='h-[100vh] w-[200px] flex-col top-0 left-0 fixed bg-black border-white border-r-2 '>
                 <h1 className="text-white text-3xl mt-10 ml-10">Cortex</h1>
                 <nav className="mt-20">
                     <NavBar />
@@ -18,7 +22,7 @@ function PageTodo() {
 
             <div id="interface-to-do" className="mt-[170px]">
                 <div className="flex justify-center">
-                    <img src={pngcentral} alt="imagem-central" />
+                    <img src={pngcentral}  alt="imagem-central" />
                     </div>
 
                 <aside className="text-center mt-3">
@@ -26,26 +30,14 @@ function PageTodo() {
                 </aside>
 
                 <section className="">
-                    <article className="fixed right-16 mt-40">
-                        <button id="addTask" className="bg-black text-green-600 text-4xl rounded-full size-12 flex align-middle justify-center text-center">+</button>
+                    <article className="fixed flex right-16 mt-40">
+                        <button id="addTask" onClick={boxForTask} className="bg-black text-green-600 text-4xl rounded-full
+                         size-12 align-middle justify-center text-center">+</button>
                     </article>
             </section>
             </div>
 
-            <div id="boxForTask" className="z-99 bg-black/80 h-full w-full justify-center align-middle left-0 top-0 fixed ">
-                <div id="boxTask" className="w-[450px] h-[560px] bg-white m-auto mt-[90px] rounded-[20px]">
-                    <div className="pt-14">
-                        <h1 className="text-center font-bold text-3xl">Tarefas</h1>
-
-                        <aside id="inputs" className="mt-20 columns-1 text-center align-middle">
-                            <input type="text" className="border-2 w-[290px] pt-[10px] placeholder:align-middle" placeholder="Título da tarefa" /><br />
-
-                            <input type="text" className="border-2 w-[290px] mt-[15px] pt-[10px] placeholder:align-middle" placeholder="Descrição da tarefa (*opcional)" />
-                        </aside>
-
-                    </div>
-                </div>
-            </div>
+            <boxForTask />
         </div>
     )
 }
